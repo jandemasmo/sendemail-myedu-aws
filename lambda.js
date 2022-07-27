224b797d9431599ec363510ce5339071e15a4013
jandemasmo <jandemasmo@yahoo.com>
2022-07-27 20:48:46 -0300

    alterações no arquivo lambada

--- lambda.js
+++ lambda.js
@@ -2,7 +2,8 @@ const nodemailer = require("nodemailer");
 
 exports.handler  = async (event, context) => {
         try {
-            if(event.name !== undefined, event.email !== undefined, event.message !== undefined){
+            const {name, email, message} = JSON.parse(event.body);
+            if(name !== undefined, email !== undefined, message !== undefined){
                 const userMail = process.env.USER_MAIL;
                 const passwordMail = process.env.PASSWORD_MAIL;
                 
@@ -15,9 +16,9 @@ exports.handler  = async (event, context) => {
                 await transporter.sendMail({
                     from: userMail,
                     to: userMail,
-                    replyTo: event.email,
-                    subject: `Você recebeu um contato de ${event.name}.`,
-                    text: event.message
+                    replyTo: email,
+                    subject: `Você recebeu um contato de ${name}.`,
+                    text: message
                 })
 
                 return{
@@ -36,17 +37,16 @@ exports.handler  = async (event, context) => {
         } catch (error) {
             return{
                 statusCode: 400,
-                body: JSON.stringify({ message: "Não foi possível enviar e-mail.", error})
+                body: JSON.stringify({ message: "Não foi possível enviar e-mail.", error}),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Headers': 'Content-Type',
+                    'Access-Control-Allow-Origin': '*',
+                    'Access-Control-Allow-Methods': 'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
+                    'Access-Control-Allow-Credentials': true,
+                }
             }
         }
         
 }
 
-// exports.handler = async (event) => {
-//     // TODO implement
-//     const response = {
-//         statusCode: 200,
-//         body: JSON.stringify('Hello from Lambda!'),
-//     };
-//     return response;
-// };
